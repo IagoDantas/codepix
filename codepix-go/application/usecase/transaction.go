4b52db7d461012f7362e27532f10419eb0756154
Iago Dantas <iagodantas.8@gmail.com>
2023-10-21 19:48:09 -0300

    grpc server

---
+++ codepix-go/application/usecase/transaction.go
@@ -0,0 +1,68 @@
+package usecase
+
+import "github.com/IagoDantas/codepix/codepix-go/domain/model"
+
+type TransactionUseCase struct {
+	TransactionRepository model.TransactionRepositoryInterface
+	PixRepository         model.PixKeyRepositoryInterface
+}
+
+func (t *TransactionUseCase) Register(accountId string, amount float64, pixKeyto string, pixKeyKindTo string, description string) (*model.Transaction, error) {
+	account, err := t.PixRepository.FindAccount(accountId)
+	if err != nil {
+		return nil, err
+	}
+	pixKey, err := t.PixRepository.FindKeyByKind(pixKeyto, pixKeyKindTo)
+	if err != nil {
+		return nil, err
+	}
+	transaction, err := model.NewTransaction(account, amount, pixKey, description)
+	if err != nil {
+		return nil, err
+	}
+	t.TransactionRepository.Save(transaction)
+	if transaction.ID == "" {
+		return nil, err
+	}
+	return transaction, nil
+}
+
+func (t *TransactionUseCase) Confirm(transactionId string) (*model.Transaction, error) {
+	transaction, err := t.TransactionRepository.Find(transactionId)
+	if err != nil {
+		return nil, err
+	}
+	transaction.Status = model.TransactionConfirmed
+	err = t.TransactionRepository.Save(transaction)
+	if err != nil {
+		return nil, err
+	}
+	return transaction, nil
+}
+
+func (t *TransactionUseCase) Complete(transactionId string) (*model.Transaction, error) {
+	transaction, err := t.TransactionRepository.Find(transactionId)
+	if err != nil {
+		return nil, err
+	}
+	transaction.Status = model.TransactionCompleted
+	err = t.TransactionRepository.Save(transaction)
+	if err != nil {
+		return nil, err
+	}
+	return transaction, nil
+}
+
+func (t *TransactionUseCase) Error(transactionId string, reason string) (*model.Transaction, error) {
+	transaction, err := t.TransactionRepository.Find(transactionId)
+	if err != nil {
+		return nil, err
+	}
+	transaction.Status = model.TransactionError
+	transaction.CancelDescription = reason
+	err = t.TransactionRepository.Save(transaction)
+	if err != nil {
+		return nil, err
+	}
+	return transaction, nil
+}
--- codepix-go/go.mod
+++ codepix-go/go.mod
@@ -2,4 +2,16 @@ module github.com/IagoDantas/codepix/codepix-go
 
 go 1.15
 
-require github.com/satori/go.uuid v1.2.0
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/satori/go.uuid v1.2.0
+	github.com/stretchr/testify v1.8.4 // indirect
+	google.golang.org/grpc v1.59.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gorm.io/driver/sqlite v1.5.4
+)
